f23cfded5cb3be5700689832bab6ffde22bcf264
Jean Viegas <jeanmelg@gmail.com>
2021-10-26 15:54:33 -0300

    mais ajustes para passar no lint

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
-const btCriarTarefa = document.getElementById('criar-tarefa')
-const listaTarefas = document.getElementById('lista-tarefas')
-const textoTarefa = document.getElementById('texto-tarefa')
-const tarefas = document.getElementsByClassName('tarefa')
-const botaoApagaTudo = document.getElementById('apaga-tudo')
-const botaoApagaCompletos = document.getElementById('remover-finalizados')
-const tarefasCompletas = document.getElementsByClassName('tarefa completed')
+const btCriarTarefa = document.getElementById('criar-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
+const textoTarefa = document.getElementById('texto-tarefa');
+const tarefas = document.getElementsByClassName('tarefa');
+const botaoApagaTudo = document.getElementById('apaga-tudo');
+const botaoApagaCompletos = document.getElementById('remover-finalizados');
+const tarefasCompletas = document.getElementsByClassName('tarefa completed');
 
-btCriarTarefa.addEventListener('click', criaTarefa)
-
-function criaTarefa(){
-  let tarefa = document.createElement('li')
-  tarefa.innerText = textoTarefa.value;
-  tarefa.className = 'tarefa'
-  tarefa.addEventListener('click', marcaTarefa)
-  tarefa.addEventListener('dblclick', completaTarefa)
-  listaTarefas.appendChild(tarefa)
-  textoTarefa.value = ''
-
-}
-
-function marcaTarefa(e){
-  for (let tarefa of tarefas){
-    tarefa.style.backgroundColor = ''
+function marcaTarefa(e) {
+  for (let tarefa of tarefas) {
+    tarefa.style.backgroundColor = '';
   }
-  e.target.style.backgroundColor = 'rgb(128, 128, 128)'
+  e.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
-function completaTarefa(e){
-  if (e.target.className == 'tarefa completed'){
-    e.target.className = 'tarefa'
+function completaTarefa(e) {
+  if (e.target.className === 'tarefa completed') {
+    e.target.className = 'tarefa';
   } else {
-    e.target.className = 'tarefa completed'
+    e.target.className = 'tarefa completed';
   }
 }
 
-botaoApagaTudo.addEventListener('click', function(){
-  listaTarefas.innerHTML = ''
-})
+function criaTarefa() {
+  const tarefa = document.createElement('li');
+  tarefa.innerText = textoTarefa.value;
+  tarefa.className = 'tarefa';
+  tarefa.addEventListener('click', marcaTarefa);
+  tarefa.addEventListener('dblclick', completaTarefa);
+  listaTarefas.appendChild(tarefa);
+  textoTarefa.value = '';
+}
+btCriarTarefa.addEventListener('click', criaTarefa);
 
-botaoApagaCompletos.addEventListener('click', function(){
-  for( let i in tarefasCompletas){
+botaoApagaTudo.addEventListener('click', () => {
+  listaTarefas.innerHTML = '';
+});
+
+botaoApagaCompletos.addEventListener('click', () => {
+  for (let i in tarefasCompletas){
     tarefasCompletas[0].remove();
   }
-})
+});
