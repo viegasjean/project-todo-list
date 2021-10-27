ccbfd0f09612c905c85a50d583362453f8d78da9
Jean Viegas <jeanmelg@gmail.com>
2021-10-27 00:59:35 -0300

    finaliza requisitos bonus

--- script.js
+++ script.js
@@ -10,14 +10,15 @@ const botaoMoverBaixo = document.getElementById('mover-baixo');
 const botaoRemoverSelecionado = document.getElementById('remover-selecionado');
 const tarefasCompletas = document.getElementsByClassName('tarefa completed');
 
+// Função para desmarcar todas as tarefas e marcar somente o alvo
 function marcaTarefa(e) {
   for (let tarefa of tarefas) {
-    tarefa.style.backgroundColor = '';
+    tarefa.id = ''
   }
-  e.target.style.backgroundColor = 'rgb(128, 128, 128)';
   e.target.id = 'selected'
 }
 
+// Função para marcar a tarefa como completa
 function completaTarefa(e) {
   if (e.target.className === 'tarefa completed') {
     e.target.className = 'tarefa';
@@ -26,60 +27,62 @@ function completaTarefa(e) {
   }
 }
 
-
+// botão para criar a tarefa e adicionar dois event listener
 btCriarTarefa.addEventListener('click', () => {
   const tarefa = document.createElement('li');
   tarefa.innerText = textoTarefa.value;
   tarefa.className = 'tarefa';
-   tarefa.addEventListener('click', marcaTarefa);
   tarefa.addEventListener('dblclick', completaTarefa);
+  tarefa.addEventListener('click', marcaTarefa);
   listaTarefas.appendChild(tarefa);
   textoTarefa.value = '';
 });
 
+// Ação para botão de apagar tudo, insere html vazio no lugar da lista
 botaoApagaTudo.addEventListener('click', () => {
   listaTarefas.innerHTML = '';
 });
 
+// Ação para apagar todas as tarefas marcadas como completas, da ultima para a primeira
 botaoApagaCompletos.addEventListener('click', () => {
-  for(let i = tarefasCompletas.length - 1; i >= 0; i--){
+  for (let i = tarefasCompletas.length - 1; i >= 0; i -= 1) {
     tarefasCompletas[i].remove()
   }
 });
 
+// Botão para converter em string e salvar o html listaTarefas no local storage
 botaoSalvaTarefas.addEventListener('click', () => {
   localStorage.setItem('lista', JSON.stringify(listaTarefas.innerHTML));
 });
 
-function carregaPagina(){
+// Ao carregar a página converte de volta para HTML e o carrega
+window.onload = function () {
   const listaSalva = JSON.parse(localStorage.getItem('lista'));
   listaTarefas.innerHTML = listaSalva;
-}
-
-window.onload = function() {
-  carregaPagina();
 };
 
 // Adiciona evento para mover a tarefa selecionada para a posição superior, se houver
 botaoMoverCima.addEventListener('click', () => {
   const tarefaSelecionada = document.getElementById('selected');
-  if (tarefaSelecionada.previousElementSibling && tarefaSelecionada){
-    listaTarefas.insertBefore(tarefaSelecionada, tarefaSelecionada.previousElementSibling);
+  if (tarefaSelecionada && tarefaSelecionada != tarefaSelecionada.parentNode.firstElementChild) {
+    tarefaSelecionada.parentNode.insertBefore(tarefaSelecionada, tarefaSelecionada.previousElementSibling);
   }
 })
+
 
 // Adiciona evento para mover a tarefa selecionada para a posição inferior, se houver
 botaoMoverBaixo.addEventListener('click', () => {
   const tarefaSelecionada = document.getElementById('selected');
-
-  if (tarefaSelecionada.nextElementSibling && tarefaSelecionada){
-    listaTarefas.insertBefore(tarefaSelecionada.nextElementSibling, tarefaSelecionada);
+  if (tarefaSelecionada && tarefaSelecionada != tarefaSelecionada.parentNode.lastElementChild) {
+    tarefaSelecionada.nextElementSibling.insertAdjacentElement('afterend', tarefaSelecionada)
   }
+
 });
 
+// Ação para remover somente a tarefa que estiver selecionada
 botaoRemoverSelecionado.addEventListener('click', () => {
   const tarefaSelecionada = document.getElementById('selected');
-  if(tarefaSelecionada){
+  if (tarefaSelecionada) {
     listaTarefas.removeChild(tarefaSelecionada);
   }
 });
